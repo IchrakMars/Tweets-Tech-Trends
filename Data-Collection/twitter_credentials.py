df1ba281b61219e397e4c870ccb94a787be1f7f4
chiche4243 <coucou_ms@hotmail.com>
2018-06-25 13:06:38 +0100

    Initial commit

---
+++ Data-Collection/twitter_credentials.py
@@ -0,0 +1,12 @@
+#--------------------------------------------------------------------------------
+# Credentials can be generated via Twitter's Application Management:
+#       https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+# Get your own Twitter Credentials through the link above, then copy and paste the given informations and fill the fields below : 
+
+consumer_key = "..."
+consumer_secret = "..."
+access_token = "..."
+access_token_secret = "..."
+
